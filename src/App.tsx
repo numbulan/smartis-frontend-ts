68b5b69dfd8d9a3852dc706f6212e382a3dddf0f
Martin Burri <martin.burri95@gmx.ch>
2022-05-04 08:54:10 +0200

    start with ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import BasicTable from "./Content/Table";
+import UserList from "./Content/UserList";
 
 function App() {
+  const [userList, setUserList]: any = useState([]);
+  /*const List = [
+    { name: "Martin", id: 1, counter: 0 },
+    { name: "Jonas", id: 2, counter: 0 },
+    { name: "Thomas", id: 3, counter: 0 },
+    { name: "Tamara", id: 4, counter: 0 },
+    { name: "Marlon", id: 5, counter: 0 },
+    { name: "Johanna", id: 6, counter: 0 },
+    { name: "Noah", id: 7, counter: 0 },
+    { name: "Kevin", id: 8, counter: 2 },
+    { name: "Christian", id: 9, counter: 5 },
+  ];*/
+  async function componentDidMount() {
+    fetch("http://localhost:3000/users")
+      .then((response) => response.json())
+      .then((res) => {
+        if (res && res.data) {
+          setUserList([...res.data]);
+        }
+      });
+  }
+  componentDidMount();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BasicTable users={userList} />
     </div>
   );
 }
---
+++ src/Content/Table.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Card from "../UI/Card";
+
+function createData(name: string, counter: number) {
+  return { name, counter };
+}
+
+const BasicTable: React.FC<{
+  users: { name: string; id: number; counter: number }[];
+}> = (props) => {
+  let rows: any = [];
+  props.users.map((user) => rows.push(createData(user.name, user.counter)));
+
+  return (
+    <Card className="table">
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>User</TableCell>
+              <TableCell align="right">Counter</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row: any) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.counter}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Card>
+  );
+};
+
+export default BasicTable;
