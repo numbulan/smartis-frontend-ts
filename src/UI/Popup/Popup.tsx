2d0f1d2b8b1b0c6e142b55cbfd3082b3aeda92a0
Martin Burri <martin.burri95@gmx.ch>
2022-06-24 13:04:07 +0200

    change popup

--- src/UI/Popup/Popup.tsx
+++ src/UI/Popup/Popup.tsx
@@ -1,6 +1,6 @@
 import SmartisForm from "../SmartisForm/SmartisForm";
 import Smartis from "../../Content/Smartis/Smartis";
-import React, { Fragment } from "react";
+import React from "react";
 import "./Popup.css";
 import { Stack } from "react-bootstrap";
 
@@ -10,30 +10,27 @@ function Backdrop(props: any) {
 
 function Popup(props: any) {
   return (
-    <Fragment>
-      <Backdrop closePopup={props.closePopup} />
-      <div className="popup">
-        <div className="popup_inner">
-          {props.content === "smartisform" ? (
-            <Stack gap={3}>
-              <h1>Smarties vergeben</h1>
-              <SmartisForm
-                closePopup={props.closePopup}
-                activeUser={props.activeUser}
-              />
-            </Stack>
-          ) : (
-            <Stack gap={3}>
-              <h1 className="">Smarties von {props.selectedUser}</h1>
-              <Smartis
-                closePopup={props.closePopup}
-                selectedUser={props.selectedUser}
-              />
-            </Stack>
-          )}
-        </div>
+    <div className="popup" onClick={props.closePopup}>
+      <div className="popup_inner">
+        {props.content === "smartisform" ? (
+          <Stack gap={3}>
+            <h1>Smarties vergeben</h1>
+            <SmartisForm
+              closePopup={props.closePopup}
+              activeUser={props.activeUser}
+            />
+          </Stack>
+        ) : (
+          <Stack gap={3}>
+            <h1 className="">Smarties von {props.selectedUser}</h1>
+            <Smartis
+              closePopup={props.closePopup}
+              selectedUser={props.selectedUser}
+            />
+          </Stack>
+        )}
       </div>
-    </Fragment>
+    </div>
   );
 }
 
